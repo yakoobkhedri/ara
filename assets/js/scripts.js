3f4fed5925e4d2f2a2c46050b978ca42be0f316b
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-12-18 15:31:16 +0330

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,40 @@
+// swiper
+var blog = new Swiper(".blog", {
+  loop: true,
+  autoplay: true,
+  slidesPerView: 1,
+  spaceBetween: 36,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    }
+  },
+});
+var comment = new Swiper(".comment", {
+  loop: true,
+  autoplay: true,
+  slidesPerView: 1,
+  spaceBetween: 36,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+});
+var costumer = new Swiper(".costumer", {
+  loop: true,
+  autoplay: true,
+  slidesPerView: 3,
+  spaceBetween: 36,
+  breakpoints: {
+    576:{
+      direction: "vertical",
+    }
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+});
