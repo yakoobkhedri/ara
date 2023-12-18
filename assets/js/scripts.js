2a45e60cc9a10193d3e1659f25986c141f8ebb0c
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-12-18 17:24:38 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,9 @@
 // swiper
 var blog = new Swiper(".blog", {
   loop: true,
-  autoplay: true,
+  autoplay: {
+    delay: 5000,
+  },
   slidesPerView: 1,
   spaceBetween: 36,
   breakpoints: {
@@ -15,26 +17,47 @@ var blog = new Swiper(".blog", {
 });
 var comment = new Swiper(".comment", {
   loop: true,
-  autoplay: true,
+  autoplay: {
+    delay: 5000,
+  },
   slidesPerView: 1,
   spaceBetween: 36,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-},
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
+  },
 });
 var costumer = new Swiper(".costumer", {
   loop: true,
-  autoplay: true,
+  autoplay: {
+    delay: 5000,
+  },
   slidesPerView: 3,
   spaceBetween: 36,
   breakpoints: {
-    576:{
+    576: {
       direction: "vertical",
     }
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-},
+  },
+});
+
+// animation
+
+ScrollReveal().reveal('.top', {
+  duration: 2000,
+  origin: 'top',
+  distance: '100px',
+  delay: 500,
+  easing: 'ease-out'
+});
+ScrollReveal().reveal('.bottom', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '100px',
+  delay: 500,
+  easing: 'ease-out'
 });
